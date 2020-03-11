653cd62a25608832b882646efe72fe8b92ae3c80
openedbox <openedbox@qq.com>
2020-03-11 15:17:37 +0800

    add lib entry

---
+++ VLCPlayer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  DeviceEventEmitter,
+  NativeModules,
+  Platform,
+  View,
+  requireNativeComponent
+} from "react-native";
+const VLCPlayer = props => {
+  return (
+    <>
+      <View></View>
+    </>
+  );
+};
+
+export default VLCPlayer;
---
+++ index.js
@@ -0,0 +1,8 @@
+
+import VLCPlayer from './VLCPlayer';
+
+const ExportBox = {
+  VLCPlayer
+}
+
+module.exports = ExportBox;

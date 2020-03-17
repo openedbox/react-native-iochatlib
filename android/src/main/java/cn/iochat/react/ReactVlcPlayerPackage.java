0d124d34330e1fa21e42cdd482538accb527e67d
openedbox <openedbox@qq.com>
2020-03-17 12:45:10 +0800

    mv package

--- android/src/main/java/cn/iochat/react/ReactVlcPlayerPackage.java
+++ android/src/main/java/cn/iochat/react/ReactVlcPlayerPackage.java
@@ -1,30 +1,30 @@
-package cn.iochat.react.wx;
+package cn.iochat.react;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+import cn.iochat.react.vlc.ReactVlcPlayerViewManager;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class WeChatPackage implements ReactPackage {
+
+public class ReactVlcPlayerPackage implements ReactPackage {
+
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Arrays.asList(new NativeModule[]{
-                // Modules from third-party
-                new WeChatModule(reactContext),
-        });
+        return Collections.emptyList();
     }
 
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Collections.emptyList();
+        return Collections.<ViewManager>singletonList(new ReactVlcPlayerViewManager());
     }
 }
--- android/src/main/java/cn/iochat/react/WeChatPackage.java
+++ android/src/main/java/cn/iochat/react/WeChatPackage.java
@@ -1,4 +1,4 @@
-package cn.iochat.react.vlc;
+package cn.iochat.react;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
@@ -6,24 +6,27 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import cn.iochat.react.wx.WeChatModule;
 
-public class ReactVlcPlayerPackage implements ReactPackage {
-
+public class WeChatPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Collections.emptyList();
+        return Arrays.asList(new NativeModule[]{
+                // Modules from third-party
+                new WeChatModule(reactContext),
+        });
     }
 
-    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Collections.<ViewManager>singletonList(new ReactVlcPlayerViewManager());
+        return Collections.emptyList();
     }
 }

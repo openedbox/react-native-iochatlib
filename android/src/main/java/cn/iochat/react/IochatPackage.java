f35a4ad1008e3f20489c695a140051ba921f184d
openedbox <openedbox@qq.com>
2020-04-02 13:30:02 +0800

    core

--- android/src/main/java/cn/iochat/react/IochatPackage.java
+++ android/src/main/java/cn/iochat/react/IochatPackage.java
@@ -22,7 +22,6 @@ public class IochatPackage implements ReactPackage {
     }
 
     // Deprecated RN 0.47
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
